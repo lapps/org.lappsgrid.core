d01988b3bfd78a0863884503088ec27b69d7643d
Keith Suderman <suderman@anc.org>
2015-02-05 19:38:49 -0500

    Added readme. Replaced references to Constants class.

--- src/main/java/org/lappsgrid/core/DataFactory.java
+++ src/main/java/org/lappsgrid/core/DataFactory.java
@@ -16,13 +16,11 @@
  */
 package org.lappsgrid.core;
 
-//import org.lappsgrid.serialization.Data;
-import org.lappsgrid.discriminator.Constants;
+import org.lappsgrid.discriminator.Discriminators;
 import org.lappsgrid.serialization.Data;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -47,7 +45,7 @@ public class DataFactory
 
    public static String ok(boolean pretty)
    {
-      Data<String> data = new Data<String>(Constants.Uri.OK);
+      Data<String> data = new Data<String>(Discriminators.Uri.OK);
       if (pretty) {
          return data.asPrettyJson();
       }
@@ -56,7 +54,7 @@ public class DataFactory
 
    public static String error(String errorMessage)
    {
-      Data<String> data = new Data<>(Constants.Uri.ERROR, errorMessage);
+      Data<String> data = new Data<>(Discriminators.Uri.ERROR, errorMessage);
       return data.asJson();
    }
 
@@ -65,7 +63,7 @@ public class DataFactory
       StringWriter swriter = new StringWriter();
       PrintWriter writer = new PrintWriter(swriter);
       error.printStackTrace(writer);
-      return new Data(Constants.Uri.ERROR, swriter.toString()).asJson();
+      return new Data(Discriminators.Uri.ERROR, swriter.toString()).asJson();
    }
 
    public static String error(String message, Throwable error)
@@ -74,12 +72,12 @@ public class DataFactory
       PrintWriter writer = new PrintWriter(swriter);
       writer.println(message);
       error.printStackTrace(writer);
-      return new Data(Constants.Uri.ERROR, swriter.toString()).asJson();
+      return new Data(Discriminators.Uri.ERROR, swriter.toString()).asJson();
    }
 
    public static String query(String queryString)
    {
-      return new Data(Constants.Uri.QUERY, queryString).asJson();
+      return new Data(Discriminators.Uri.QUERY, queryString).asJson();
    }
 
 //   public static Data jquery(String queryString) {
@@ -96,22 +94,22 @@ public class DataFactory
 
    public static String get(String id)
    {
-      return new Data(Constants.Uri.GET, id).asJson();
+      return new Data(Discriminators.Uri.GET, id).asJson();
    }
 
    public static String list()
    {
-      return new Data(Constants.Uri.LIST).asJson();
+      return new Data(Discriminators.Uri.LIST).asJson();
    }
 
    public static String index(String[] items)
    {
       if (items.length == 0)
       {
-         return new Data(Constants.Uri.STRING_LIST, "").asJson();
+         return new Data(Discriminators.Uri.STRING_LIST, "").asJson();
       }
       List<String> list = Arrays.asList(items);
-      return new Data<>(Constants.Uri.STRING_LIST, list).asJson();
+      return new Data<>(Discriminators.Uri.STRING_LIST, list).asJson();
 //      StringBuilder buffer = new StringBuilder();
 //      buffer.append(items[0]);
 //      for (int i = 1; i < items.length; ++i)
@@ -119,25 +117,25 @@ public class DataFactory
 //         buffer.append(" ");
 //         buffer.append(items[i]);
 //      }
-//      return new Data(Constants.Uri.INDEX, buffer.toString());
+//      return new Data(Discriminators.Uri.INDEX, buffer.toString());
    }
 
 //   public static Data index(String items)
 //   {
-//      return new Data(Constants.Uri.INDEX, items);
+//      return new Data(Discriminators.Uri.INDEX, items);
 //   }
 
    public static String stringList(String[] items)
    {
       List<String> list = Arrays.asList(items);
-      return new Data<List<String>>(Constants.Uri.STRING_LIST, list).asJson();
+      return new Data<List<String>>(Discriminators.Uri.STRING_LIST, list).asJson();
 //      StringBuilder buffer = new StringBuilder(4096);
 //      for (String item : items)
 //      {
 //         buffer.append(item);
 //         buffer.append('\n');
 //      }
-//      return new Data(Constants.Uri.STRING_LIST, buffer.toString());
+//      return new Data(Discriminators.Uri.STRING_LIST, buffer.toString());
    }
 
    public static Data stringList(List<String> list)
@@ -148,57 +146,57 @@ public class DataFactory
 //         buffer.append(item);
 //         buffer.append('\n');
 //      }
-//      return new Data(Constants.Uri.STRING_LIST, buffer.toString());
-      return new Data(Constants.Uri.STRING_LIST, list);
+//      return new Data(Discriminators.Uri.STRING_LIST, buffer.toString());
+      return new Data(Discriminators.Uri.STRING_LIST, list);
    }
 
    public static String text(String text)
    {
-      return new Data(Constants.Uri.TEXT, text).asJson();
+      return new Data(Discriminators.Uri.TEXT, text).asJson();
    }
 
    public static String document(String document)
    {
-      return new Data(Constants.Uri.DOCUMENT, document).asJson();
+      return new Data(Discriminators.Uri.DOCUMENT, document).asJson();
    }
 
    public static String xml(String xml)
    {
-      return new Data(Constants.Uri.XML, xml).asJson();
+      return new Data(Discriminators.Uri.XML, xml).asJson();
    }
 
    public static String gateDocument(String document)
    {
-      return new Data(Constants.Uri.GATE, document).asJson();
+      return new Data(Discriminators.Uri.GATE, document).asJson();
    }
 
    public static String gate(String document)
    {
-      return new Data(Constants.Uri.GATE, document).asJson();
+      return new Data(Discriminators.Uri.GATE, document).asJson();
    }
 
    public static String oneperline(String text)
    {
-      return new Data(Constants.Uri.ONE_PER_LINE, text).asJson();
+      return new Data(Discriminators.Uri.ONE_PER_LINE, text).asJson();
    }
 
    public static String opl(String text)
    {
-      return new Data(Constants.Uri.ONE_PER_LINE, text).asJson();
+      return new Data(Discriminators.Uri.ONE_PER_LINE, text).asJson();
    }
 
    public static String json(String text)
    {
-      return new Data(Constants.Uri.JSON, text).asJson();
+      return new Data(Discriminators.Uri.JSON, text).asJson();
    }
 
    public static String jsonLD(String text)
    {
-      return new Data(Constants.Uri.JSON_LD, text).asJson();
+      return new Data(Discriminators.Uri.JSON_LD, text).asJson();
    }
 
-   public static String lapps(String json) { return new Data(Constants.Uri.LAPPS, json).asJson(); }
+   public static String lapps(String json) { return new Data(Discriminators.Uri.LAPPS, json).asJson(); }
 
-   public static String meta(String json) { return new Data(Constants.Uri.META, json).asJson(); }
+   public static String meta(String json) { return new Data(Discriminators.Uri.META, json).asJson(); }
 }
 
