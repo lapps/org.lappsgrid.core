a40b88c5eac3aa6afbfa151adbfc6c16909e5e93
Keith Suderman <suderman@cs.vassar.edu>
2016-01-22 16:41:16 -0500

    Ready for Maven Central release.

---
+++ secure.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script adds encrypted environment variables to the .travis.yml
+# file. The passwords and/or passphrases are read from the user's home directory
+# so no private credentials are exposed by including this in SCM.
+source $HOME/.passphrases
+
+KEYS="GH_USER GH_PASS SONATYPE_USERNAME SONATYPE_PASSWORD PGP_PASSPHRASE ENCRYPTION_PASSWORD"
+REPO=$(git remote show origin | grep Push | cut -d/ -f4,5 | sed 's/\.git$//g')
+
+function encrypt() {
+	name=$1
+	eval value=\$$name
+	echo "Encrypting $name=$value"
+	travis encrypt -r $REPO $name=\"$value\" --add
+}
+
+echo "Encoding keys for the repository $REPO"
+for key in $KEYS; do
+	encrypt $key
+done
+
+echo ".travis.yml updated."
